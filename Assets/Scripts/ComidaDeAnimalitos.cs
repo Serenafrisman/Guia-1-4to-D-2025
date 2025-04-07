78aaf8c121c7d800d338c0d6db5dfccaa1870e62
Serenafrisman <49008981@est.ort.edu.ar>
2025-04-07 10:42:51 -0300

    terminar comida de animalitos

---
+++ Assets/Scripts/ComidaDeAnimalitos.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComidaDeAnimalitos : MonoBehaviour
+
+{
+    public string codigo;
+    public int diasQuevaAQudarse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (codigo == "G" && diasQuevaAQudarse >3)
+        {
+
+        }
+        else if (codigo == "PP" && diasQuevaAQudarse > 3)
+        {
+
+        }else if (codigo == "PG" && diasQuevaAQudarse > 3)
+        {
+
+        }
+        else
+        {
+            Debug.Log("El código y/o dias de estadía no son válidos");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ConversionDeMonedas.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversionDeVariables : MonoBehaviour
+{
+    //1. Declaración de variables
+
+    public float montoAConvertir;
+    public string codigoMoneda;
+
+    float montoConvertido;
+    float montoMinimoAConvertir = 1000;
+
+    float cotizacionMonedaElegida;
+    float contizacionDolar = 1297.21f;
+    float cotizacionEuro = 1621.2f;
+    float cotizacionReal = 183.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //2.Ingreso y validación de datos
+
+        if (montoAConvertir < montoMinimoAConvertir) 
+        {
+            Debug.Log("El monto mínimo es $1000");
+            return;
+        }
+
+        if (codigoMoneda == "E")
+        {
+
+            cotizacionMonedaElegida = 1621.2f;
+        }else if (codigoMoneda=="D")
+        {
+            cotizacionMonedaElegida = 1297.21f;
+        }else if (codigoMoneda=="R")
+        {
+            cotizacionMonedaElegida = 183.0f;
+        }
+        else
+        {
+            Debug.Log("Opción de moneda extranjera no válida");
+        }
+        //3.Procesamiento de datos
+
+        montoConvertido = montoAConvertir / cotizacionMonedaElegida;
+
+        //4.Salida de datos
+        Debug.Log("La conversión de ese monto de pesos equivale a " + montoConvertido);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
